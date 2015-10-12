b42de549b8c809ce8c1ad0ea39cd04929e69ea01
Arié <alancry@gpartner.eu>
2015-10-12 18:33:15 +0200

    Delete file .iml, not working , and starting main - Arie

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import imh.TweetWord;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        TweetWord tweet = new TweetWord("hello it's arie", 121);
+        System.out.println(tweet.getWord() + " " + tweet.getPonderation());
     }
 }
