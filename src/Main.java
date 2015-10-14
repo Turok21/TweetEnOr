3a2404ee868547ed212c510c9fb76d09ab6ea687
Arié <alancry@gpartner.eu>
2015-10-14 12:41:39 +0200

    Maj Constructeur Ctrl tweet en or - arie

--- src/Main.java
+++ src/Main.java
@@ -12,24 +12,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("CLASS MAIN IS NOW RUNNING !");
 
-        TweetWord alpha = new TweetWord("alpha", 10);
-        TweetWord beta = new TweetWord("beta", 20);
-        TweetWord gamma = new TweetWord("gamma", 30);
-        TweetWord delta = new TweetWord("delta", 40);
-
-        List<TweetWord> myTweetList = new ArrayList<>();
-        myTweetList.add(alpha);
-        myTweetList.add(beta);
-        myTweetList.add(gamma);
-        myTweetList.add(delta);
-
-        KeyWord myKeyWord = new KeyWord("Ski", myTweetList);
-        CtrlTweetEnOr ctrlTweet = new CtrlTweetEnOr(myKeyWord);
-
-//        System.out.println(tweet);
-//        System.out.println(myTweetWordList);
-//        System.out.println(WordComparator.wordCompare(alpha.getWord(), beta.getWord()));
-
-        System.out.println(ctrlTweet.isMotValid("wazaa"));
     }
 }
--- src/controllers/CtrlTweetEnOr.java
+++ src/controllers/CtrlTweetEnOr.java
@@ -3,6 +3,7 @@ package controllers;
 import utils.KeyWord;
 import utils.TweetWord;
 import utils.WordComparator;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -11,27 +12,40 @@ import java.util.ListIterator;
  */
 
 public class CtrlTweetEnOr {
-    private KeyWord keyWords;
-    private List<String> proposedWords;
+    private KeyWord      _keyWords;
+    private List<String> _proposedWords;
 
-    public CtrlTweetEnOr(KeyWord keyWords) {
-        this.keyWords = keyWords;
+    public CtrlTweetEnOr(String word) {
+        // TODO ARIE : DELETE
+        TweetWord alpha = new TweetWord("alpha", 10);
+        TweetWord beta = new TweetWord("beta", 20);
+        TweetWord gamma = new TweetWord("gamma", 30);
+        TweetWord delta = new TweetWord("delta", 40);
+
+        List<TweetWord> myTweetList = new ArrayList<>();
+        myTweetList.add(alpha);
+        myTweetList.add(beta);
+        myTweetList.add(gamma);
+        myTweetList.add(delta);
+        // TODO ARIE : Fin delete
+
+        this._keyWords = new KeyWord(word, myTweetList);
     }
 
     public KeyWord getKeyWords() {
-        return keyWords;
+        return _keyWords;
     }
 
     public List<String> getProposedWords() {
-        return proposedWords;
+        return _proposedWords;
     }
 
     public String getOneProposedWords(int index) {
-        return this.proposedWords.get(index);
+        return this._proposedWords.get(index);
     }
 
     public void addWordsToList(String newWord) {
-        proposedWords.add(newWord);
+        _proposedWords.add(newWord);
     }
 
     public void foundWords() {
@@ -39,7 +53,7 @@ public class CtrlTweetEnOr {
     }
 
     public TweetWord isMotValid(String word) {
-        ListIterator<TweetWord> listIterator = this.keyWords.getListWords().listIterator();
+        ListIterator<TweetWord> listIterator = this._keyWords.getListWords().listIterator();
         while (listIterator.hasNext()) {
             TweetWord next = listIterator.next();
             // TODO : Arie supprimer le println
@@ -52,11 +66,11 @@ public class CtrlTweetEnOr {
     }
 
     public List<TweetWord> getListWords() {
-        return this.keyWords.getListWords();
+        return this._keyWords.getListWords();
     }
 
     public boolean isMotAlreadyUse(String Mot) {
-        ListIterator listIterator = this.proposedWords.listIterator();
+        ListIterator listIterator = this._proposedWords.listIterator();
         // TODO CPE : loop sur l'iterateur et verification de MOT sur chacun des words de proposedWord
         return false;
     }
