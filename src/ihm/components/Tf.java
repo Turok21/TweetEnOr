04754e62b134e23dab08c9616ba4cfbb50ac47de
Guilhem EYRAUD <guilhemeyraud@hotmail.fr>
2015-11-09 12:21:28 +0100

    fix tft

--- src/ihm/components/Tf.java
+++ src/ihm/components/Tf.java
@@ -91,6 +91,19 @@ public class Tf extends JTextField {
 	
 	public void setwh(float w,float h){
 		setSize(new Dimension((int) w,(int) h));
+		if(_gravity == CENTER){
+	    	_center_x = getWidth()/2;
+	    	_center_y = getHeight()/2;
+	    }else if(_gravity == TOP_RIGHT){
+	    	_center_x = getWidth();
+	    	_center_y = 0;
+	    }else if(_gravity == TOP_LEFT){
+	    	_center_x = 0;
+	    	_center_y = 0;
+	    }else{
+	    	_center_x = getWidth()/2;
+	    	_center_y = getHeight()/2;
+	    }
 	}
 	public void setw(float w){
 		setwh((int) w,getHeight());
