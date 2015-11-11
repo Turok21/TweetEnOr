743c64ea1647e61b297067c34bc3848a17ff40bc
Amoki <hugo.duroux@gmail.com>
2015-11-11 19:00:08 +0100

    commentaires

--- src/utils/TweetParser.java
+++ src/utils/TweetParser.java
@@ -60,6 +60,7 @@ public abstract class TweetParser {
 	public static KeyWord findWords(String keyWord, final Shared_component shared) {
     	// Récupération des tweets
     	List<String> listTweets = getTweets(keyWord, shared);
+    	// Mise à jour de la barre de progression
     	shared._progressbar.setMaximum(listTweets.size());
     	shared._progressbar.setValue(0);
     	final List<String> words = new ArrayList<>();
@@ -76,13 +77,16 @@ public abstract class TweetParser {
 		    	  shared._progressbar.setValue(shared._progressbar.getValue()+1);
 		    	  shared.txt_line1.settext("Traitement des tweets... (" + shared._progressbar.getValue() + "/" + shared._progressbar.getMaximum() + ")");
 		    	  if(!excludedTypes.contains(pos.split(":")[0])) {
+		    		  // Si la nature du mot n'est pas à exclure, on le rejoute dans la liste
 		    		  words.add(token);
 		    	  }
 		      }
 		    });
 		    for (String tweet : listTweets) {
+		    	// On nettoie de tweet (suppréssion des Emoji et autres symboles inutiles
 		    	tweet = cleanTweet(tweet);
-		    	tt.process(tweet.split("‘|’|'|\\s+")); // Convertion de la phrase en une liste de mots
+		    	// Convertion de la phrase en une liste de mots
+		    	tt.process(tweet.split("‘|’|'|\\s+"));
 	        }
 		  }
 		catch (Exception e) {
@@ -92,8 +96,8 @@ public abstract class TweetParser {
 		  tt.destroy();
 		}
 		
-		
-		List<String> cleanedWords = cleanWords(words, keyWord);
+		// Nettoyage des mots (suppréssion des majuscules, accents, caractère spéciaux)
+		List<String> cleanedWords = cleanWords(words, keyWord); 
 
         Map<String, Integer> topWords = listWordToPonderatedMap(cleanedWords);
         // ponderation
@@ -102,8 +106,9 @@ public abstract class TweetParser {
     }
 
     private static List<String> getTweets(String keyWord, Shared_component shared) {
+    	// Récupération des identifiants Twitter
         TwitterFactory tf = new TwitterFactory(config().build());
-        Twitter twitter = tf.getInstance(); //création de l'objet twitter 
+        Twitter twitter = tf.getInstance();
         List<String> listTweets = new ArrayList<>();
 
         shared._progressbar.setMaximum(nbTweetsToGet);
@@ -111,8 +116,8 @@ public abstract class TweetParser {
         shared.txt_line1.settext("Récupération des tweets ... (" + shared._progressbar.getValue() + "/"+shared._progressbar.getMaximum() + ")");
         Query query = new Query(keyWord + " exclude:retweets");
 
-        query.setLang("fr"); // On ne récup que les tweet en français
-        query.count(100);
+        query.setLang("fr"); // On ne récup que les tweets en français
+        query.count(100); // 100 tweets/requete
         QueryResult result;
         try {
             result = twitter.search(query);
@@ -138,18 +143,19 @@ public abstract class TweetParser {
 
     private static Map<String, Integer> listWordToPonderatedMap(List<String> words) {
         Map<String, Integer> ponderatedWords = new HashMap<>();
-        /** generate map<word, occurences> */
+        // Génération d'une map<mot, nombre_occurence)
         for (String word : words) {
             int count = ponderatedWords.containsKey(word) ? ponderatedWords.get(word) : 0;
             ponderatedWords.put(word, count + 1);
         }
 
-        /** Tri de la map */
+        // Tri de la map par ordre décroissant d'occurence
         Map<String, Integer> sortPonderatedWord = sortByComparator(ponderatedWords);
-
+        
+        // Fusion des mots similaires dans la map
         Map<String, Integer> sortMergedPonderatedWord = mergeSimilarWords(sortPonderatedWord);
         
-        /** Recuperation des 10 mots les plus représentatifs */
+        //Recuperation des 10 mots les plus représentatifs 
         Map<String, Integer> topPonderatedWord = new HashMap<>();
         int nbWord = 10;
         for (Map.Entry<String, Integer> entry : sortMergedPonderatedWord.entrySet()) {
@@ -164,22 +170,25 @@ public abstract class TweetParser {
     	Map<String, Integer> mergedMapWords = new HashMap<>();
     	
     	for (String word : new ArrayList<>(sortedMapWords.keySet())) {
+    		// On parcourt la map triée des mots. 
+    		// La map triée nous permet de s'assurer qu'en cas de fusion de 2 mots, on garde l'orthographe du plus utilisé
     		boolean match = false;
     		for (String mergedWord : new ArrayList<>(mergedMapWords.keySet())) {
+    			// Pour chaque mot, on vérifie si un équivalent est déjà présent
     			if(WordComparator.wordCompare(word, mergedWord)) {
     				match = true;
-    				// add old and new ponderation
+    				// aSi oui, on ajoute la pondération du mot courant à celle du mot équivalent
     				int newPonderation =  mergedMapWords.get(mergedWord) + sortedMapWords.get(word);
     				mergedMapWords.put(mergedWord, newPonderation);
     				break;
     			}
     		}
     		if(!match) {
-    			// No merge available, create new entry
+    			// Si aucun mot équivalent n'a été trouvé, on ajoute de mot à la map
     			mergedMapWords.put(word, sortedMapWords.get(word));
     		}
     	}
-    	// Sort again if order has changed
+    	// On tri de nouveau la map
     	return sortByComparator(mergedMapWords);
     }
 
@@ -194,6 +203,7 @@ public abstract class TweetParser {
         /** Creation des objects TweetWord, et ajout dans la listTweetWord **/
         double value;
         int totalSet = 0;
+        // On veut que la somme des points attribué à chaque mot soit égale à 100
         for (Map.Entry<String, Integer> entry : tweetList.entrySet()) {
             value = entry.getValue() * 100 / total;
             value = Math.round(value);
@@ -201,7 +211,8 @@ public abstract class TweetParser {
             TweetWord tweetWord = new TweetWord(entry.getKey(), (int) value);
             listTweetWord.add(tweetWord);
         }
-
+        // Il arrive que la somme des points ne soit pas égale à 100, a cause des arrondis
+        // Dans ce cas, on arrtibut les points manquant aux x premiers de la liste. X étant le nombre de points manquants
         if (totalSet != 100) {
             int difference = 100 - totalSet;
             Iterator<TweetWord> iMap = listTweetWord.iterator();
@@ -219,7 +230,9 @@ public abstract class TweetParser {
         List<String> cleanedWords = new ArrayList<>();
         for (String word : strings) {
             word = cleanWord(word);
-            // Remove useless words
+            // On exclut les mots dont on ne veut pas (cf la liste stopwords)
+            // On les mots de deux lettres ou moins
+            // On exclu les urls
             if (stopwords.contains(word) || word.length() <= 2 || word.equals(keyWord.toLowerCase()) || word.equals(urlToken)) {
                 continue;
             }
@@ -229,13 +242,15 @@ public abstract class TweetParser {
     }
 
     public static String cleanWord(String word) {
+    	// On supprime les accents, on retire tous les caratères spéciaux sauf les - et on met tout au minuscule
     	word = Normalizer.normalize(word, Normalizer.Form.NFD);
         return word.toLowerCase().replaceAll("[^a-z]|^-", "");
     }
 
     private static ConfigurationBuilder config() {
+    	// Configuration du module chargé de requeter l'API Twitter
         Map<String, String> env = System.getenv();
-        ConfigurationBuilder conf = new ConfigurationBuilder(); // connexion au twitter
+        ConfigurationBuilder conf = new ConfigurationBuilder();
         conf.setOAuthConsumerKey(env.get("CONSUMER_KEY"));
         conf.setOAuthConsumerSecret(env.get("CONSUMER_SECRET"));
         conf.setOAuthAccessToken("1215866521-LjqUbYcP7n9zCHctz9pGbUiA8UMYKHUY3eCHOuM");
@@ -244,6 +259,7 @@ public abstract class TweetParser {
     }
 
     private static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap) {
+    	// Methode de tri d'une map par valeur décroissante
         List<Map.Entry<String, Integer>> list = new LinkedList<>(unsortMap.entrySet());
         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> o1,
@@ -263,13 +279,14 @@ public abstract class TweetParser {
     private static String cleanTweet(String tweet) {
     	String cleanedTweet = removeEmojiAndSymbolFromString(tweet);
     	cleanedTweet = cleanedTweet
-    			.replaceAll("http(s)?://[^ ]+", " " + urlToken) // add a space cause twitter can concat a word and an url without problem
-    			.replaceAll("#", "")
-    			.replaceAll("\n", " ")
-    			.replaceAll("\r", " ")
-    			.replaceAll("\"", "")
-    			.replaceAll("@[^ ]+", "")
-    			.replaceAll("…", "");
+    			// On remplace les url par un token identifiable (pour garder la structure grammaicale de la phrase)
+    			.replaceAll("http(s)?://[^ ]+", " " + urlToken) // On ajoute un espace car Twitter permet d'acoller une mot et u
+    			.replaceAll("#", "") // On enlève les hashtag devant les mots
+    			.replaceAll("\n", " ") // On enlève les retours ligne
+    			.replaceAll("\r", " ") // On enlève les retours ligne
+    			.replaceAll("\"", "") // On enlève les guillemets *
+    			.replaceAll("@[^ ]+", "") // On enlève les mensions (@le_compte_d_une_personne)
+    			.replaceAll("…", ""); // On enlève le caractère 3-points très utilisé sur twitter (un seul char au leu de 3)
     	return cleanedTweet;
     }
     
