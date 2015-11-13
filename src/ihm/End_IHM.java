eb48f94792e4ca0714f7ad03d774c7c82053486b
Azali <azali.mmadi@gmail.com>
2015-11-13 05:54:22 +0100

    commentaire
    commentaire

--- src/ihm/End_IHM.java
+++ src/ihm/End_IHM.java
@@ -19,16 +19,21 @@ import ihm.components.composent.GRAVITY;
 import utils.TweetWord;
 import Sounds.Player;
 
+/**
+ * 
+ * @author Azali
+ *
+ */
 public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 	
 	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	private Bt _b_again;
-	private String _hastag;
-	private int _points;
+	private static final long serialVersionUID = 1L; // Serelisation Id 
+	private Bt _b_again;  // restart button
+	private String _hastag; // hashtag used for the game
+	private int _points; // nb point gained
 
 	public static void main(String[] args){		
 		
@@ -37,6 +42,14 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 		new End_IHM(new JFrame(""),0,listword,"test",666);
 	}
 
+	/**
+	 * 
+	 * @param fram the maine JFrame
+	 * @param fin did She win (1) or loose(0)
+	 * @param listword liste of the word she had to found
+	 * @param hastag hashtag played
+	 * @param point nb point gained
+	 */
 	public End_IHM(JFrame fram,int fin,ArrayList<TweetWord> listword,String hastag,int point) {
 		super();
 		_hastag = hastag;
@@ -49,7 +62,7 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 		}
 	    
 	   show_windows();
-	   
+	   // displaying different songs 
 	   if(fin == 0){
 			new Thread(new Runnable() {
 				public void run() {
@@ -70,14 +83,19 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 	   
 	}
 
-	
+	/**
+	 * 
+	 * @param fram the main JFrame
+	 * @param fin she loose
+	 * @param listword  listword liste of the word she had to found
+	 */
 	private void loose_screen(JFrame fram,int fin,ArrayList<TweetWord> listword){
 		
 		_jp_principal = load_fenetre_and_panel_principale("Un Tweet en Or - Fin ","fond_Fail.jpg",fram, false);
 		
 		_fenetre.addKeyListener(this);
 			
-	    boolean left = true;
+	    boolean left = true; // playe the word at the leste
 	    float lastY = 23;
 	    boolean firtWordAtLef = true;
 	    boolean firtWordAtright = true;
@@ -86,14 +104,14 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 	    
 		for(TweetWord word : listword){		
 		
-			
+			// foreach word creating a new Txt and setting different properties
 			Txt txt = new Txt(""+word.getWord());
 			txt.setForeground(new Color(87, 1, 8 ,255));
 			txt.setFont(arista_light.deriveFont(Font.TRUETYPE_FONT,35));
 			txt.setGravity(GRAVITY.CENTER);
 			txt.auto_resize();
 			
-			
+			//displaying the hashtag
 			Txt hash = new Txt("#"+_hastag);
 	        hash.setFont(arista.deriveFont(Font.TRUETYPE_FONT,82));
 	        hash.setForeground(new Color(140, 0, 0 ,255));
@@ -101,7 +119,7 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 	        hash.setxy(20, 12);
 	        _jp_principal.add(hash);
 	        
-	        
+	        // displaying the points she gained 
 	        Txt point = new Txt("Score : "+_points);
 	        point.setFont(arista.deriveFont(Font.TRUETYPE_FONT,82));
 	        point.setForeground(new Color(140, 0, 0 ,255));
@@ -110,16 +128,16 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 	        _jp_principal.add(point);
 	        
 	        
-			
+			// absolute positioning of all word
 			if(left)
 			{
 				if(firtWordAtLef)
-				{
+				{ // the first to be displayed
 					txt.setxy(11,  lastY);
 					firtWordAtLef = false;
 				}
 				else
-				{
+				{   // Adjusting all positioning to symmetric
 					if(k == 5)
 					{
 						txt.setxy(11, ( 23 + k*6) + k );
@@ -172,6 +190,7 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 			k++;
 		}
 	    
+		// restart button
 		_b_again = new Bt();
 		_b_again.addActionListener(this);
 		_b_again.setText("Recommencer");
@@ -183,6 +202,12 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 			
 	}
 
+	/**
+	 * 
+	 * @param fram the main JFrame
+	 * @param fin she wan
+	 * @param listword liste of all xord
+	 */
 	
 	private void win_screen(JFrame fram,int fin,ArrayList<TweetWord> listword){
 		
@@ -190,13 +215,14 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 		
 		_fenetre.addKeyListener(this);
 		
+		// displaying fancy images
 		Txt imgGagne = new Txt(new ImageIcon("./data/images/gagne.png"));
         imgGagne.setGravity(GRAVITY.CENTER);
         imgGagne.setxy(50, 12);
         imgGagne.auto_resize();
         _jp_principal.add(imgGagne);
 
-
+        //displaying the hashtag
         Txt hash = new Txt("#"+_hastag);
         hash.setFont(arista.deriveFont(Font.TRUETYPE_FONT,82));
         hash.setForeground(new Color(255, 209, 0 ,255));
@@ -204,6 +230,7 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
         hash.setxy(50, 24);
         _jp_principal.add(hash);
         
+     // displaying the points she gained 
         Txt point = new Txt("Score : "+_points);
         point.setFont(arista.deriveFont(Font.TRUETYPE_FONT,82));
         point.setForeground(new Color(255, 209, 0 ,255));
@@ -217,14 +244,14 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 	    float lastX = 10;
 	    float lastY = 75;
 		for(TweetWord word : listword){		
-					    
+			// foreach word creating a new Txt and setting different properties
 			Txt txt = new Txt(""+word.getWord());
 			txt.setFont(arista_light.deriveFont(Font.TRUETYPE_FONT,40));
 			txt.setForeground(new Color(242, 209, 0 ,255));
 			txt.setGravity(GRAVITY.CENTER);
 			txt.auto_resize();
 			txt.setxy(lastX, lastY);
-
+			// absolute positioning of all word
 			lastX += 9;
 			if(k < 5  )
 				lastY -= 7;
@@ -234,7 +261,7 @@ public class End_IHM extends IHM_Iterface implements ActionListener,KeyListener{
 			_jp_principal.add(txt);
 			k++;
 		}
-		
+		// restart button
 		_b_again = new Bt();
 		_b_again.addActionListener(this);
 		_b_again.setFont(arista_light.deriveFont(Font.TRUETYPE_FONT,40));
