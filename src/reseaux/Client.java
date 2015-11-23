12b8c0d43b40e040f628c2eca9519a1cbe9f3a3b
Amoki <hugo.duroux@gmail.com>
2015-11-23 16:45:06 +0100

    add constructor for Client and Server

--- src/reseaux/Client.java
+++ src/reseaux/Client.java
@@ -11,6 +11,24 @@ public class Client {
 
     public static Socket socket = null;
     public static Thread th1;
+    public static DataExchange de;
+
+    public Client(String url, int port) {
+        System.out.println("Initialisation de la connexion");
+        try {
+            socket = new Socket(Server.url, Server.port);
+            System.out.println("Connexion établie avec le serveur");
+            de = new DataExchange(socket);
+            th1 = new Thread(de);
+            th1.start();
+        } catch (IOException e) {
+            System.err.println("Aucun serveur à l'écoute du port " + socket.getLocalPort());
+        }
+    }
+
+    public DataExchange getDataExchange() {
+        return Client.de;
+    }
 
     public static void main(String[] args) {
         try {
@@ -19,7 +37,7 @@ public class Client {
             socket = new Socket(Server.url, Server.port);
 
             System.out.println("Connexion établie avec le serveur"); // Si le message s'affiche c'est que je suis connecté
-            DataExchange de = new DataExchange(socket);
+            de = new DataExchange(socket);
             th1 = new Thread(de);
             th1.start();
 
--- src/reseaux/Server.java
+++ src/reseaux/Server.java
@@ -15,6 +15,26 @@ public class Server {
     private static Socket       socket = null;
 
     public static Thread th;
+    public static DataExchange de;
+
+    public Server(int port) {
+        System.out.println("Initialisation de la connexion");
+        try {
+            ss = new ServerSocket(port);
+            System.out.println("Le serveur est à l'écoute du port " + ss.getLocalPort());
+            socket = ss.accept();
+            System.out.println("Un client s'est connecté");
+            de = new DataExchange(socket);
+            th = new Thread(de);
+            th.start();
+        } catch (IOException e) {
+            System.err.println("Le port " + ss.getLocalPort() + " est déjà utilisé !");
+        }
+    }
+
+    public DataExchange getDataExchange() {
+        return Client.de;
+    }
 
     public static void main(String[] args) {
         try {
@@ -22,7 +42,7 @@ public class Server {
             System.out.println("Le serveur est à l'écoute du port " + ss.getLocalPort());
             socket = ss.accept();
             System.out.println("Un client s'est connecté");
-            DataExchange de = new DataExchange(socket);
+            de = new DataExchange(socket);
             th = new Thread(de);
             th.start();
             de.emit(DataType.TEST, "Ceci est un envoie de données");
