8e1d1c90c32ff8b494ffe494ab18142f4a49c065
Guilhem EYRAUD <guilhemeyraud@hotmail.fr>
2015-12-07 11:18:57 +0100

    minor

--- src/ihm/components/Shared_component.java
+++ src/ihm/components/Shared_component.java
@@ -11,9 +11,9 @@ import reseaux.DataType;
 public class Shared_component {
 	public JProgressBar _progressbar;// progressbar du loader lors du chargement des données de jeux
 	public Txt txt_line1;// label d'info utilisateur du loader lors du chargement des données de jeux
-	public boolean is_message;
+	public boolean _is_message;
 	public DataType _datatype; 
-	public HashMap<DataType , Object> data_hash;
+	public HashMap<DataType , Object> _data_hash;
 	
 	
 	public Shared_component() {
