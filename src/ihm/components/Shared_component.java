2b74ccd27612506d782f6d38d0fdbad21bdd9c1a
Guilhem EYRAUD <guilhemeyraud@hotmail.fr>
2015-12-07 11:08:31 +0100

    mino

--- src/ihm/components/Shared_component.java
+++ src/ihm/components/Shared_component.java
@@ -2,9 +2,14 @@ package ihm.components;
 
 import javax.swing.JProgressBar;
 
+import reseaux.DataType;
+
+
+
 public class Shared_component {
 	public JProgressBar _progressbar;// progressbar du loader lors du chargement des données de jeux
 	public Txt txt_line1;// label d'info utilisateur du loader lors du chargement des données de jeux
+	
 	
 	public Shared_component() {
 		_progressbar = new JProgressBar();
