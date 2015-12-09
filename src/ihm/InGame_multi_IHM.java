171cad34cc63caf05bc727245f71b1e9c22a91fb
Guilhem EYRAUD <guilhemeyraud@hotmail.fr>
2015-12-09 17:31:52 +0100

    yy

--- src/ihm/InGame_multi_IHM.java
+++ src/ihm/InGame_multi_IHM.java
@@ -178,7 +178,7 @@ public class InGame_multi_IHM extends InGame_IHM{
 			new Thread(new Runnable() {
 				@Override
 				public void run() {
-					long limit = 150;
+					long limit = 10;
 					long t0 = System.currentTimeMillis();
 					long old = 0;
 					_go_timer = true;
